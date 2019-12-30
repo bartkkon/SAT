3fcbdc79332764212fea761f8ca636fae3b2ce86
Acio <konrad1bartkowiak@gmial.com>
2019-12-30 11:03:09 +0100

    Dev1. CreateClass

---
+++ Saving Akcelerator Tool/Klasy/User/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saving_Accelerator_Tool.Klasy.User
+{
+    class User
+    {
+    }
+}
