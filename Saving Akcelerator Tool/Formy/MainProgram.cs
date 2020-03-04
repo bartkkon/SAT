4dc7e7c959259f1856e32c3936fb956b49cc831c
Patryk <patmal23@gmail.com>
2020-03-04 15:11:04 +0100

    Removed extra string "version"
    test

--- Saving Akcelerator Tool/Formy/MainProgram.cs
+++ Saving Akcelerator Tool/Formy/MainProgram.cs
@@ -57,7 +57,7 @@ namespace Saving_Accelerator_Tool
                 }
                 else
                 {
-                    toolStripStatusLabel1.Text = "0.5.0.32  Beta Version Portable Version";
+                    toolStripStatusLabel1.Text = "0.5.0.32  Beta Portable Version";
                 }
 
                 if (Environment.UserName.ToString() == "BartkKon")
